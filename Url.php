ccbe48057a994e4fb12ca784c79ef66af84c4b97
EncoreBot <ghbot@encoredigitalgroup.com>
2024-04-14 13:45:44 +0000

    [StdLib] Create Url Class and Url Helper Functions

---
+++ Url.php
@@ -0,0 +1,1 @@
+<?phpnamespace EncoreDigitalGroup\StdLib;class Url{    public function encode(mixed $data): string    {        return urlencode($data);    }    public function decode(string $data): string    {        return urldecode($data);    }}
