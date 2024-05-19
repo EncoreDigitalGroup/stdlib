9d759c6abe011e867b58b3d2555fc02ee860e3e7
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-05-18 21:26:52 -0500

    Create file_copy() (#3)

--- src/ObjectHelpers/file_helpers.php
+++ src/ObjectHelpers/file_helpers.php
@@ -4,9 +4,18 @@ use EncoreDigitalGroup\StdLib\Exceptions\FileNotFoundException;
 use EncoreDigitalGroup\StdLib\Objects\File;
 
 if (!function_exists('file_save_contents')) {
+    /** @deprecated use file_put_contents() instead */
     function file_save_contents(string $path, mixed $data): void
     {
         file_put_contents($path, $data);
+    }
+}
+
+if (!function_exists('file_copy')) {
+    /** @throws FileNotFoundException */
+    function file_copy(string $source, string $destination): void
+    {
+        File::copy($source, $destination);
     }
 }
 
