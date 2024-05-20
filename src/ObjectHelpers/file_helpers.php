0c84af8186898da39445328c1b556a35cc1d00fd
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-05-20 13:32:40 -0500

    Created Additional File Helpers

--- src/ObjectHelpers/file_helpers.php
+++ src/ObjectHelpers/file_helpers.php
@@ -26,3 +26,38 @@ if (!function_exists('file_delete')) {
         File::delete($path);
     }
 }
+
+if (!function_exists('file_last_accessed')) {
+    function file_last_accessed(string $path): false|int
+    {
+        return fileatime($path);
+    }
+}
+
+if (!function_exists('file_last_changed')) {
+    function file_last_changed(string $path): false|int
+    {
+        return filectime($path);
+    }
+}
+
+if (!function_exists('file_last_modified')) {
+    function file_last_modified(string $path): false|int
+    {
+        return filemtime($path);
+    }
+}
+
+if (!function_exists('file_size')) {
+    function file_size(string $path): false|int
+    {
+        return filesize($path);
+    }
+}
+
+if (!function_exists('file_type')) {
+    function file_type(string $path): false|string
+    {
+        return filetype($path);
+    }
+}
