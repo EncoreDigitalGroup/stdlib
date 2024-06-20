33eeecb3eb7476d81b42252eaa87026ea13d73f0
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-06-20 09:04:35 -0500

    [Exceptions] Organize FilesystemExceptions

--- src/Exceptions/DirectoryNotFoundException.php
+++ src/Exceptions/DirectoryNotFoundException.php
@@ -2,18 +2,14 @@
 
 namespace EncoreDigitalGroup\StdLib\Exceptions;
 
+use EncoreDigitalGroup\StdLib\Exceptions\FilesystemExceptions\DirectoryNotFoundException as BaseDirectoryNotFoundException;
 use EncoreDigitalGroup\StdLib\Objects\ExitCode;
 
-class DirectoryNotFoundException extends BaseException
+/** @deprecated use EncoreDigitalGroup\StdLib\Exceptions\FilesystemExceptions\DirectoryNotFoundException instead */
+class DirectoryNotFoundException extends BaseDirectoryNotFoundException
 {
     public function __construct(?string $path = null)
     {
-        if (is_null($path)) {
-            $msg = str_concat_space('Unable to locate directory on line', $this->line, 'in', $this->file);
-        } else {
-            $msg = str_concat_space('Unable to locate directory', $path, 'on line', $this->line, 'in', $this->file);
-        }
-
-        parent::__construct($msg, ExitCode::RESOURCE_UNAVAILABLE, $this->getPrevious());
+        parent::__construct($msg);
     }
 }
--- src/Exceptions/FileNotFoundException.php
+++ src/Exceptions/FileNotFoundException.php
@@ -2,18 +2,14 @@
 
 namespace EncoreDigitalGroup\StdLib\Exceptions;
 
+use EncoreDigitalGroup\StdLib\Exceptions\FilesystemExceptions\FileNotFoundException as BaseFileNotFoundException;
 use EncoreDigitalGroup\StdLib\Objects\ExitCode;
 
-class FileNotFoundException extends BaseException
+/** @deprecated use EncoreDigitalGroup\StdLib\Exceptions\FilesystemExceptions\FileNotFoundException instead. */
+class FileNotFoundException extends BaseFileNotFoundException
 {
     public function __construct(?string $path = null)
     {
-        if (is_null($path)) {
-            $msg = str_concat_space('Unable to locate file on line', $this->line, 'in', $this->file);
-        } else {
-            $msg = str_concat_space('Unable to locate file', $path, 'on line', $this->line, 'in', $this->file);
-        }
-
-        parent::__construct($msg, ExitCode::RESOURCE_UNAVAILABLE, $this->getPrevious());
+        parent::__construct($msg);
     }
 }
