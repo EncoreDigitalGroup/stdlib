5dd5ec7d7474496fd776e6781315d6ea9fe4174a
Marc Beinder <marc@marcbeinder.com>
2024-08-18 13:38:21 -0500

    Update enum_helpers.php

--- src/ObjectHelpers/enum_helpers.php
+++ src/ObjectHelpers/enum_helpers.php
@@ -1,13 +1,34 @@
 <?php
 
 if (!function_exists('enum')) {
-    function enum(UnitEnum $enum): string|int
+    function enum(UnitEnum $enum): int|string
     {
         if ($enum instanceof BackedEnum) {
             return $enum->value;
         }
 
         return $enum->name;
+    }
+}
 
+if (!function_exists('enum_string')) {
+    function enum_string(UnitEnum $enum): string
+    {
+        if ($enum instanceof IntBackedEnum) {
+            throw new InvalidArgumentException("IntBackedEnum Prohibited.");
+        }
+
+        return $enum->name;
+    }
+}
+
+if (!function_exists('enum_int')) {
+    function enum_int(UnitEnum $enum): string
+    {
+        if ($enum instanceof StringBackedEnum) {
+            throw new InvalidArgumentException("StringBackEnum Prohibited.");
+        }
+
+        return $enum->name;
     }
 }
