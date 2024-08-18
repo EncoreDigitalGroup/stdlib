152016614a21532818f6ea38b35ccb66ca4e2b92
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2024-08-18 13:46:14 -0500

    Backed Enum Helpers (#15)
    Co-authored-by: onairmarc <onairmarc@users.noreply.github.com>

--- src/ObjectHelpers/enum_helpers.php
+++ src/ObjectHelpers/enum_helpers.php
@@ -1,13 +1,34 @@
 <?php
 
 if (!function_exists('enum')) {
-    function enum(UnitEnum $enum): string|int
+    function enum(UnitEnum $enum): int|string
     {
         if ($enum instanceof BackedEnum) {
             return $enum->value;
         }
 
         return $enum->name;
+    }
+}
 
+if (!function_exists('enum_string')) {
+    function enum_string(BackedEnum $enum): string
+    {
+        if (is_int($enum->value)) {
+            throw new InvalidArgumentException('IntBackedEnum Prohibited.');
+        }
+
+        return $enum->name;
+    }
+}
+
+if (!function_exists('enum_int')) {
+    function enum_int(BackedEnum $enum): string
+    {
+        if (is_string($enum->value)) {
+            throw new InvalidArgumentException('StringBackEnum Prohibited.');
+        }
+
+        return $enum->name;
     }
 }
