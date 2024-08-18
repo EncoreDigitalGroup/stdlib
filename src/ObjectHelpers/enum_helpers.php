c709aedfb70105dd006ffbbbf8f82d9716db9614
onairmarc <onairmarc@users.noreply.github.com>
2024-08-18 18:45:35 +0000

    Dusting

--- src/ObjectHelpers/enum_helpers.php
+++ src/ObjectHelpers/enum_helpers.php
@@ -15,7 +15,7 @@ if (!function_exists('enum_string')) {
     function enum_string(BackedEnum $enum): string
     {
         if (is_int($enum->value)) {
-            throw new InvalidArgumentException("IntBackedEnum Prohibited.");
+            throw new InvalidArgumentException('IntBackedEnum Prohibited.');
         }
 
         return $enum->name;
@@ -26,7 +26,7 @@ if (!function_exists('enum_int')) {
     function enum_int(BackedEnum $enum): string
     {
         if (is_string($enum->value)) {
-            throw new InvalidArgumentException("StringBackEnum Prohibited.");
+            throw new InvalidArgumentException('StringBackEnum Prohibited.');
         }
 
         return $enum->name;
