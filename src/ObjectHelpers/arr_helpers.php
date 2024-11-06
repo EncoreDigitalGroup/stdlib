8e6ce2cdadf4c8144b6cc1a1a726797ab8df6344
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-11-06 08:56:20 -0600

    [ObjectHelpers] Deprecate arr Functions

--- src/ObjectHelpers/arr_helpers.php
+++ src/ObjectHelpers/arr_helpers.php
@@ -6,6 +6,7 @@ use EncoreDigitalGroup\StdLib\Objects\Arr;
 if (!function_exists('arr_to_short_syntax')) {
     /**
      * @throws ArgumentNullException
+     * @deprecated No Replacement
      */
     function arr_to_short_syntax(array|string $value): array|string
     {
