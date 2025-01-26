065611695e2356cd6dc8b5b5f6cfe8b51f720e1d
EncoreBot <ghbot@encoredigitalgroup.com>
2025-01-26 20:29:09 +0000

    Dusting

--- src/Objects/Arr.php
+++ src/Objects/Arr.php
@@ -5,6 +5,4 @@ namespace EncoreDigitalGroup\StdLib\Objects;
 use EncoreDigitalGroup\StdLib\Support\Types\Arr as BaseArray;
 
 /** @deprecated Use EncoreDigitalGroup\StdLib\Support\Types\Arr instead. */
-class Arr extends BaseArray
-{
-}
+class Arr extends BaseArray {}
--- src/Objects/ExitCode.php
+++ src/Objects/ExitCode.php
@@ -3,6 +3,4 @@
 namespace EncoreDigitalGroup\StdLib\Objects;
 
 /** @deprecated Use EncoreDigitalGroup\StdLib\Objects\Filesystem\ExitCode instead. */
-class ExitCode extends Filesystem\ExitCode
-{
-}
+class ExitCode extends Filesystem\ExitCode {}
