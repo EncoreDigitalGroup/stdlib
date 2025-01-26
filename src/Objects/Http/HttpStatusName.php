5a44835145aeddfd3450d1032d0b65c8720a2c90
Marc Beinder <marc@marcbeinder.com>
2025-01-26 08:55:00 -0600

    [Objects] Create HttpStatusName

---
+++ src/Objects/Http/HttpStatusName.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EncoreDigitalGroup\StdLib\Objects\Http;
+
+/**
+ * @api
+ *
+ * @codeCoverageIgnore
+ */
+class HttpStatusName
+{
+    const OK = "OK";
+    const CREATED = "Created";
+    const ACCEPTED = "Accepted";
+    const NO_CONTENT = "No Content";
+    const MULTIPLE_CHOICES = "Multiple Choices";
+    const MOVED_PERMANENTLY = "Moved Permanently";
+    const FOUND = "Found";
+    const PERMANENT_REDIRECT = "Permanent Redirect";
+    const TEMPORARY_REDIRECT = "Temporary Redirect";
+    const SEE_OTHER = "See Other";
+    const NOT_MODIFIED = "Not Modified";
+    const BAD_REQUEST = "Bad Request";
+    const UNAUTHORIZED = "Unauthorized";
+    const NOT_LICENSED = "Not Licensed";
+    const FORBIDDEN = "Forbidden";
+    const NOT_FOUND = "Not Found";
+    const METHOD_NOT_ALLOWED = "Method Not Allowed";
+    const CONFLICT = "Conflict";
+    const GONE = "Gone";
+    const PRECONDITION_FAILED = "Precondition Failed";
+    const PAYLOAD_TOO_LARGE = "Payload Too Large";
+    const UNSUPPORTED_MEDIA_TYPE = "Unsupported Media Type";
+    const TOO_MANY_REQUESTS = "Too Many Request";
+    const INTERNAL_SERVER_ERROR = "Internal Server Error";
+    const BAD_GATEWAY = "Bad Gateway";
+    const SERVICE_UNAVAILABLE = "Service Unavailable";
+    const GATEWAY_TIMEOUT = "Gateway Timeout";
+    const NETWORK_AUTHENTICATION_REQUIRED = "Network Authentication Required";
+}
