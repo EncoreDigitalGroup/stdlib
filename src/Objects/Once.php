7cc9ad8ff0e076cb7a9d471b43273bf3277e1f8a
Marc Beinder <marc@marcbeinder.com>
2025-02-14 10:07:42 -0600

    Ignore Code Coverage for Illuminate Wrappers

--- src/Objects/Once.php
+++ src/Objects/Once.php
@@ -4,6 +4,7 @@ namespace EncoreDigitalGroup\StdLib\Objects;
 
 use Illuminate\Support\Once as BaseOnce;
 
+/** @codeCoverageIgnore Ignored since this is a wrapper around an Illuminate class */
 class Once
 {
     public static function handle(callable $callable): mixed
