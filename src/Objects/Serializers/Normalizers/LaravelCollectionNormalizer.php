d743ba14ef300ebe57bdcaeae174661ddecee118
EncoreBot <ghbot@encoredigitalgroup.com>
2025-02-17 15:33:09 +0000

    Dusting

--- src/Objects/Serializers/Normalizers/LaravelCollectionNormalizer.php
+++ src/Objects/Serializers/Normalizers/LaravelCollectionNormalizer.php
@@ -2,29 +2,29 @@
 
 namespace EncoreDigitalGroup\StdLib\Objects\Serializers\Normalizers;
 
+use Illuminate\Support\Collection;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Illuminate\Support\Collection;
 
 /** @experimental */
-class LaravelCollectionNormalizer implements NormalizerInterface, DenormalizerInterface
+class LaravelCollectionNormalizer implements DenormalizerInterface, NormalizerInterface
 {
-    public function normalize(mixed $data, string $format = null, array $context = []): array
+    public function normalize(mixed $data, ?string $format = null, array $context = []): array
     {
         return $data->toArray();
     }
 
-    public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
     {
         return $data instanceof Collection;
     }
 
-    public function denormalize(mixed $data, string $type, string $format = null, array $context = []): Collection
+    public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): Collection
     {
         return new Collection($data);
     }
 
-    public function supportsDenormalization(mixed $data, string $type, string $format = null, array $context = []): bool
+    public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
     {
         return $type === Collection::class;
     }
