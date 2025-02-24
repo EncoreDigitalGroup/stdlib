b2cab5ce909a448b564d6db8315e6c4a84a36cd2
Marc Beinder <marc@marcbeinder.com>
2025-02-23 20:14:34 -0600

    Refactor LaravelCollectionNormalizer

--- src/Objects/Serializers/Normalizers/LaravelCollectionNormalizer.php
+++ src/Objects/Serializers/Normalizers/LaravelCollectionNormalizer.php
@@ -3,15 +3,24 @@
 namespace EncoreDigitalGroup\StdLib\Objects\Serializers\Normalizers;
 
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /** @experimental */
-class LaravelCollectionNormalizer implements DenormalizerInterface, NormalizerInterface
+readonly class LaravelCollectionNormalizer implements DenormalizerInterface, NormalizerInterface
 {
+    public function __construct(private NormalizerInterface&DenormalizerInterface $normalizer)
+    {
+    }
+
     public function normalize(mixed $data, ?string $format = null, array $context = []): array
     {
-        return $data->toArray();
+        if (!$data instanceof Collection) {
+            throw new InvalidArgumentException('Data must be an instance of Collection to normalize.');
+        }
+
+        return array_map(fn($item) => $this->normalizer->normalize($item, $format, $context), $data->all());
     }
 
     public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
@@ -21,18 +30,29 @@ class LaravelCollectionNormalizer implements DenormalizerInterface, NormalizerIn
 
     public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): Collection
     {
-        return new Collection($data);
+        if (!is_iterable($data)) {
+            throw new InvalidArgumentException('Data must be iterable to denormalize into a Collection.');
+        }
+
+        $items = array_map(function ($value) use ($format, $context) {
+            return is_iterable($value)
+                ? $this->normalizer->denormalize($value, Collection::class, $format, $context)
+                : $value;
+        }, is_array($data) ? $data : iterator_to_array($data));
+
+        return new Collection($items);
     }
 
     public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
     {
-        return $type === Collection::class;
+        return is_a($type, Collection::class, true);
     }
 
     public function getSupportedTypes(?string $format): array
     {
         return [
             Collection::class => true,
+            "Illuminate\Support\Collection::*" => true,
         ];
     }
 }
