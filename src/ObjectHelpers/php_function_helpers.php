1baedb655c5667dc1c5bb14940090d2b0aa9971b
Marc Beinder <marc@marcbeinder.com>
2025-03-01 12:43:49 -0600

    Add Object Support to get_type

--- src/ObjectHelpers/php_function_helpers.php
+++ src/ObjectHelpers/php_function_helpers.php
@@ -4,6 +4,10 @@ if (!function_exists("get_type")) {
     /** @phpstan-ignore-next-line */
     function get_type(mixed $value): string
     {
+        if (is_object($value)) {
+            return get_class($value);
+        }
+
         return gettype($value);
     }
 }
