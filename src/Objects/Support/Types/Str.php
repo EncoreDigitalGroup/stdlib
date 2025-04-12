3aaa13a55ff539b1e7333651f77744c621fba8f5
Marc Beinder <marc@marcbeinder.com>
2025-04-12 16:56:26 -0500

    Add Str::formattedTitleCase()

--- src/Objects/Support/Types/Str.php
+++ src/Objects/Support/Types/Str.php
@@ -18,7 +18,7 @@ class Str extends StringSupport
 
     public static function toString(mixed $value): string
     {
-        return (string) $value;
+        return (string)$value;
     }
 
     public static function maxLength(string $value, ?int $length = null): string
@@ -36,5 +36,16 @@ class Str extends StringSupport
         $orFormatted = Str::replace(" Or ", " or ", $andFormatted);
 
         return Str::replace(" Of ", " of ", $orFormatted);
+    }
+
+    public static function formattedTitleCase(string $value): array|string
+    {
+        $underScoresRemoved = Str::replace("_", " ", Str::title($value));
+
+        if (!is_string($underScoresRemoved)) {
+            return $underScoresRemoved;
+        }
+
+        return Str::conjunctions($underScoresRemoved);
     }
 }
