e3d0f850eef1665164c8f97aec9608882628c1ee
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-05-03 08:03:17 -0500

    Add Arr::renameKey() and Arr::addAfter

--- src/Objects/Support/Types/Arr.php
+++ src/Objects/Support/Types/Arr.php
@@ -10,4 +10,37 @@ class Arr extends ArraySupport
     {
         return [];
     }
+
+    public static function renameKey(array $array, string $oldKey, string $newKey): array
+    {
+        if (!array_key_exists($oldKey, $array)) {
+            return $array;
+        }
+
+        $keys = array_keys($array);
+        $index = array_search($oldKey, $keys);
+
+        $keys[$index] = $newKey;
+        $values = array_values($array);
+
+        return array_combine($keys, $values);
+    }
+
+    public static function addAfter(array $array, string $afterKey, string $newKey, mixed $newValue): array
+    {
+        if (!array_key_exists($afterKey, $array)) {
+            return $array;
+        }
+
+        $newArray = [];
+        foreach ($array as $key => $value) {
+            $newArray[$key] = $value;
+
+            if ($key === $afterKey) {
+                $newArray[$newKey] = $newValue;
+            }
+        }
+
+        return $newArray;
+    }
 }
