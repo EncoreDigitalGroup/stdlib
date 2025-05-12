a30f51acd7c4602b2bc0c5355ba7a06b76c4f8a4
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-05-12 10:09:09 -0500

    Restore Laravel 10 Support with Version Checks (#69)
    Co-authored-by: EncoreBot <ghbot@encoredigitalgroup.com>

--- src/Objects/Deferred.php
+++ src/Objects/Deferred.php
@@ -2,6 +2,9 @@
 
 namespace EncoreDigitalGroup\StdLib\Objects;
 
+use EncoreDigitalGroup\StdLib\Exceptions\MissingMinimumDependencyException;
+use EncoreDigitalGroup\StdLib\Support\Internal\Composer\Composer;
+
 use function Illuminate\Support\defer;
 
 use Illuminate\Support\Defer\DeferredCallback;
@@ -11,6 +14,10 @@ class Deferred
 {
     public static function handle(?callable $callback = null, ?string $name = null, bool $always = false): DeferredCallback
     {
+        if (version_compare(Composer::version(Composer::ILLUMINATE_SUPPORT), "11.24.0", "<")) {
+            throw new MissingMinimumDependencyException(Composer::ILLUMINATE_SUPPORT);
+        }
+
         return defer($callback, $name, $always);
     }
 }
--- src/Objects/Once.php
+++ src/Objects/Once.php
@@ -2,6 +2,8 @@
 
 namespace EncoreDigitalGroup\StdLib\Objects;
 
+use EncoreDigitalGroup\StdLib\Exceptions\MissingMinimumDependencyException;
+use EncoreDigitalGroup\StdLib\Support\Internal\Composer\Composer;
 use Illuminate\Support\Once as BaseOnce;
 
 /** @codeCoverageIgnore Ignored since this is a wrapper around an Illuminate class */
@@ -9,11 +11,22 @@ class Once
 {
     public static function handle(callable $callable): mixed
     {
+        self::version();
+
         return once($callable);
     }
 
     public static function flush(): void
     {
+        self::version();
+
         BaseOnce::instance()->flush();
+    }
+
+    private static function version(): void
+    {
+        if (version_compare(Composer::version(Composer::ILLUMINATE_SUPPORT), "11.0.0", "<")) {
+            throw new MissingMinimumDependencyException(Composer::ILLUMINATE_SUPPORT);
+        }
     }
 }
