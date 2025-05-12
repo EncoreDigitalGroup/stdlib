365b0fbf2cc1f2fc07b99c2e56adb9e6c0e37397
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-05-12 09:51:36 -0500

    Restore Laravel 10 Support with Version Checks

--- src/Objects/Once.php
+++ src/Objects/Once.php
@@ -2,6 +2,9 @@
 
 namespace EncoreDigitalGroup\StdLib\Objects;
 
+use Composer\InstalledVersions;
+use EncoreDigitalGroup\StdLib\Exceptions\MissingMinimumDependencyException;
+use EncoreDigitalGroup\StdLib\Support\Internal\Composer\Composer;
 use Illuminate\Support\Once as BaseOnce;
 
 /** @codeCoverageIgnore Ignored since this is a wrapper around an Illuminate class */
@@ -9,11 +12,22 @@ class Once
 {
     public static function handle(callable $callable): mixed
     {
+        self::version();
+
         return once($callable);
     }
 
     public static function flush(): void
     {
+        self::version();
+
         BaseOnce::instance()->flush();
+    }
+
+    private static function version(): void
+    {
+        if (version_compare(Composer::version(Composer::ILLUMINATE_SUPPORT), "11.0.0", "<")) {
+            throw new MissingMinimumDependencyException(Composer::ILLUMINATE_SUPPORT);
+        }
     }
 }
