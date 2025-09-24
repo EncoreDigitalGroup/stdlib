8c9ed48b845cd1e66bc06d37058e317a3e558c8e
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-09-23 20:32:59 -0500

    Add YesNo Enum

---
+++ src/Objects/Support/Enums/YesNo.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Rights Reserved.
+ */
+
+namespace EncoreDigitalGroup\StdLib\Objects\Support\Enums;
+
+use EncoreDigitalGroup\StdLib\Objects\Support\Traits\HasEnumValue;
+
+enum YesNo: string
+{
+    use HasEnumValue;
+
+    case Yes = "yes";
+    case No = "no";
+}
