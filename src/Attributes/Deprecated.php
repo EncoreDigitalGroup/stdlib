72902430b4d6ce2151fcb2e0e233e12bed8745d2
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2025-11-03 07:06:22 -0600

    Deprecate the Deprecated Attribute in favor of PHP 8.4 Deprecated Attribute

--- src/Attributes/Deprecated.php
+++ src/Attributes/Deprecated.php
@@ -5,4 +5,5 @@ namespace EncoreDigitalGroup\StdLib\Attributes;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
+/** @deprecated Use PHP 8.4 Deprecated Attribute instead. */
 class Deprecated {}
