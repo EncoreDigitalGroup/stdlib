c1c106293dc252b7bd21fe557a241b2101e72603
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2025-12-12 14:07:44 -0600

    Refactor parameter type hints in Macroable::__callStatic() method (#168)
    Co-authored-by: EncoreBot <ghbot@encoredigitalgroup.com>

--- rector.php
+++ rector.php
@@ -1,12 +1,33 @@
 <?php
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Rights Reserved.
+ */
 
 declare(strict_types=1);
 
 use EncoreDigitalGroup\StdLib\Support\Internal\Rector\Rector;
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddMethodCallBasedStrictParamTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ParamTypeByMethodCallTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ParamTypeByParentCallTypeRector;
+
+function skipTypeHintsForMacroable(): array
+{
+    $paramTypeRules = array_filter([
+        AddMethodCallBasedStrictParamTypeRector::class,
+        ParamTypeByMethodCallTypeRector::class,
+        ParamTypeByParentCallTypeRector::class,
+    ], "class_exists");
+
+    return array_fill_keys($paramTypeRules, [
+        "*Macroable.php::__callStatic",
+    ]);
+}
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . "/src",
     ])
-    ->withRules(Rector::rules());
+    ->withRules(Rector::rules())
+    ->withSkip(skipTypeHintsForMacroable());
