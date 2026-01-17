5c2330998a6353dae2cc267b6deeef5e5d7a8c63
EncoreBot <ghbot@encoredigitalgroup.com>
2026-01-17 16:16:37 +0000

    Dusting

--- src/Objects/Support/Traits/Macroable.php
+++ src/Objects/Support/Traits/Macroable.php
@@ -88,9 +88,10 @@ trait Macroable
     }
 
     /**
-     * @param string $method
-     * @param array $parameters
+     * @param  string  $method
+     * @param  array  $parameters
      * @return mixed
+     *
      * @throws ReflectionException
      */
     public static function __callStatic($method, $parameters)
@@ -121,8 +122,8 @@ trait Macroable
     }
 
     /**
-     * @param string $method
-     * @param array $parameters
+     * @param  string  $method
+     * @param  array  $parameters
      * @return mixed
      *
      * @throws ReflectionException
