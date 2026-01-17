9e892866ab8388abe3c629d40d6c1e486aa92a78
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2026-01-17 09:55:20 -0600

    Add Lock class for managing named locks with timeout support

---
+++ src/Objects/Support/Lock.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Copyright (c) 2026. Encore Digital Group.
+ * All Rights Reserved.
+ */
+
+namespace EncoreDigitalGroup\StdLib\Objects\Support;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Sleep;
+
+class Lock
+{
+    /** @var array<string, bool> */
+    private static array $locks = [];
+
+    /**
+     * Execute a callback while holding a named lock
+     *
+     * @template T
+     * @param string $name The name of the lock to acquire
+     * @param int $timeout Maximum number of seconds to wait for the lock
+     * @param Closure(): T $callback The callback to execute while holding the lock
+     * @return T
+     */
+    public static function block(string $name, int $timeout, Closure $callback): mixed
+    {
+        self::acquire($name, $timeout);
+
+        try {
+            return $callback();
+        } finally {
+            self::release($name);
+        }
+    }
+
+    private static function acquire(string $name, int $timeout): void
+    {
+        $attempts = 0;
+
+        while (self::isLocked($name) && $attempts < $timeout) {
+            Sleep::for(1)->seconds();
+            $attempts++;
+        }
+
+        self::$locks[$name] = true;
+    }
+
+    private static function release(string $name): void
+    {
+        unset(self::$locks[$name]);
+    }
+
+    private static function isLocked(string $name): bool
+    {
+        return self::$locks[$name] ?? false;
+    }
+}
